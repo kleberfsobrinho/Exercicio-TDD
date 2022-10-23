4b260c4f092c19c8efbb863272fb8476596cf00b
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2022-10-23 15:16:42 -0300

    refactor: calculate method

--- Calculadora-Salario/src/calculator/Calculator.java
+++ Calculadora-Salario/src/calculator/Calculator.java
@@ -1,12 +1,17 @@
 package calculator;
 
 import model.Employee;
+import model.Role;
 
 public class Calculator {
     public static Double calculateSalary(Employee employee) {
-        if (employee.getSalary() < 3000)
-            return employee.getSalary() - employee.getSalary() * 0.10;
+        if (employee.getRole().equals(Role.DESENVOLVEDOR)) {
+            if (employee.getSalary() < 3000)
+                return employee.getSalary() - employee.getSalary() * 0.10;
 
-        return employee.getSalary() - employee.getSalary() * 0.20;
+            return employee.getSalary() - employee.getSalary() * 0.20;
+        }
+
+        return employee.getSalary() - employee.getSalary() * 0.15;
     }
 }
