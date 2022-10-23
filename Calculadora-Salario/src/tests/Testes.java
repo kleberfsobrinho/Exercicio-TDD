a4ac5ea800bf19e916cf849287b08dc6ed781b45
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2022-10-23 15:13:47 -0300

    test: add dba role calculate test

--- Calculadora-Salario/src/tests/Testes.java
+++ Calculadora-Salario/src/tests/Testes.java
@@ -35,4 +35,11 @@ public class Testes {
         netSalary = Calculator.calculateSalary(employeeGreater);
         assertEquals(2400.8, netSalary);
     }
+
+    @Test
+    public void calculateSalaryDBALessThan2000() {
+        Employee employee = new Employee("Alicent Hightower", "alicent.hightower@gmail.com", 1999.99, Role.DBA);
+        Double netSalary = Calculator.calculateSalary(employee);
+        assertEquals(1699.9915, netSalary);
+    }
 }
