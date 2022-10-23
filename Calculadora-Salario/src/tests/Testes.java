25dffe34ea5a3b97bd587069e3ed85c100c377bb
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2022-10-23 14:28:45 -0300

    test: improve employee initial test

--- Calculadora-Salario/src/tests/Testes.java
+++ Calculadora-Salario/src/tests/Testes.java
@@ -8,11 +8,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Testes {
     @Test
     public void createEmployee() {
-        Employee employee = new Employee("Rhaenyra Targaryen", "rhaenyra.targaryen@gmail.com", 2000, "Cargo");
+        Employee employee = new Employee("Rhaenyra Targaryen", "rhaenyra.targaryen@gmail.com", 2000, Role.DESENVOLVEDOR);
 
         assertEquals("Rhaenyra Targaryen", employee.getName());
         assertEquals("rhaenyra.targaryen@gmail.com", employee.getEmail());
         assertEquals(2000, employee.getSalary());
     }
-
 }
