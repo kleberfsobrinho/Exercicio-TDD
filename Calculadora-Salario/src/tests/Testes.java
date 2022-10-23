23121d78158bf9cd699807c3886dad1571fbfa15
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2022-10-23 14:57:31 -0300

    test: add calculate test

--- Calculadora-Salario/src/tests/Testes.java
+++ Calculadora-Salario/src/tests/Testes.java
@@ -2,6 +2,7 @@ package tests;
 
 import model.Employee;
 import model.Role;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,5 +16,12 @@ public class Testes {
         assertEquals("rhaenyra.targaryen@gmail.com", employee.getEmail());
         assertEquals(2000, employee.getSalary());
         assertEquals(Role.DESENVOLVEDOR, employee.getRole());
+    }
+
+    @Test
+    public void calculateSalaryDesenvolvedorLessThan3000() {
+        Employee employee = new Employee("Rhaenyra Targaryen", "rhaenyra.targaryen@gmail.com", 2999, Role.DESENVOLVEDOR);
+        Double netSalary = Calculator.calculateSalary(employee);
+        assertEquals(employee.getSalary() - employee.getSalary() * 0.1, netSalary);
     }
 }
