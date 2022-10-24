d4e52842122efe1fc8e878f8fb506e4d142ab342
Gabrielly Trajano <gabrielly.amorin@ccc.ufcg.edu.br>
2022-10-24 18:03:53 -0300

    create fatura model

--- Gerador-NotaFiscal/model/Bill.java
+++ Gerador-NotaFiscal/model/Bill.java
@@ -1,5 +1,33 @@
 package model;
 
 public class Bill {
+	private String name;
+    private String address;
+    private Service service;
+    private Double salary;
+
+	public Bill(String name, String address, Service service, Double salary) {
+		this.name = name;
+        this.address = address;
+        this.service = service;
+        this.salary = salary;
+	}
+	
+	public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public Double getSalary() {
+        return this.salary;
+    }
+
+    public Service getService() {
+        return this.service;
+    }
+}
 
 }
