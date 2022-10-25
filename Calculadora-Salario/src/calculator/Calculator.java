ce3b3ffe3b81a502a88cf737c1c3899ab99d380f
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2022-10-24 21:20:37 -0300

    refactor: calculate method

--- Calculadora-Salario/src/calculator/Calculator.java
+++ Calculadora-Salario/src/calculator/Calculator.java
@@ -4,25 +4,38 @@ import model.Employee;
 import model.Role;
 
 public class Calculator {
-    public static Double calculateSalary(Employee employee) {
-        if (employee.getRole().equals(Role.DESENVOLVEDOR)) {
-            if (employee.getSalary() < 3000)
-                return employee.getSalary() - employee.getSalary() * 0.10;
+    static final Double DESENVOLVEDOR_THRESHOLD = 3000.00;
+    static final Double DBA_TESTADOR_THRESHOLD = 2000.00;
+    static final Double GERENTE_THRESHOLD = 5000.00;
+    static final Double DESENVOLVEDOR_SALARY_LESS_THRESHOLD = 0.10;
+    static final Double DESENVOLVEDOR_SALARY_MORE_THRESHOLD = 0.20;
+    static final Double DBA_TESTADOR_SALARY_LESS_THRESHOLD = 0.15;
+    static final Double DBA_TESTADOR_SALARY_MORE_THRESHOLD = 0.25;
+    static final Double GERENTE_SALARY_LESS_THRESHOLD = 0.20;
+    static final Double GERENTE_SALARY_MORE_THRESHOLD = 0.30;
 
-            return employee.getSalary() - employee.getSalary() * 0.20;
+    public static Double calculateSalary(Employee employee) {
+        Double percentage;
+
+        if (employee.getRole().equals(Role.DESENVOLVEDOR)) {
+            if (employee.getSalary() < DESENVOLVEDOR_THRESHOLD)
+                percentage = DESENVOLVEDOR_SALARY_LESS_THRESHOLD;
+            else
+                percentage = DESENVOLVEDOR_SALARY_MORE_THRESHOLD;
         }
         else if (employee.getRole().equals(Role.DBA) || employee.getRole().equals(Role.TESTADOR)) {
-            if (employee.getSalary() < 2000)
-                return employee.getSalary() - employee.getSalary() * 0.15;
-
-            return employee.getSalary() - employee.getSalary() * 0.25;
+            if (employee.getSalary() < DBA_TESTADOR_THRESHOLD)
+                percentage = DBA_TESTADOR_SALARY_LESS_THRESHOLD;
+            else
+                percentage = DBA_TESTADOR_SALARY_MORE_THRESHOLD;
         }
         else {
-            if (employee.getSalary() < 5000)
-                return employee.getSalary() - employee.getSalary() * 0.20;
-
-            return employee.getSalary() - employee.getSalary() * 0.30;
+            if (employee.getSalary() < GERENTE_THRESHOLD)
+                percentage = GERENTE_SALARY_LESS_THRESHOLD;
+            else
+                percentage = GERENTE_SALARY_MORE_THRESHOLD;
         }
 
+        return employee.getSalary() - employee.getSalary() * percentage;
     }
 }
